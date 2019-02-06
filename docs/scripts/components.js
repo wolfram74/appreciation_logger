fd0c85b877e9e00246cd1271685abe3034a5ec0b
wolfram74 <phaugen@gmail.com>
2019-02-06 11:22:14 -0700

    setting up some data loading aspects

--- docs/scripts/components.js
+++ docs/scripts/components.js
@@ -1,7 +1,19 @@
-Vue.component('gratitude_input', {
+Vue.component('gratitude_body', {
   template:`
     <div>
       hello
     </div>
-  `
+  `,
+  data: function(){return load()},
+  methods:{
+    setCurrentEpochDay: function(){
+      var msPerHour = 1000*60*60
+      var msPerDay =  msPerHour*24
+      var now = Date.now()
+      var surplus = now%msPerDay
+      var dayNow = now-surplus
+      console.log(this.settings.currentEpochDay)
+    }
+  }
+
 })
--- docs/scripts/runner.js
+++ docs/scripts/runner.js
@@ -1,1 +1,16 @@
-new Vue({el: "#gratitude-input"})
+function load(){
+  var loadedData = JSON.parse(
+    localStorage.getItem('dailyLoggerState')
+    ) || {loggable:{}, settings:{currentEpochDay:0, daysReviewed:7, lastOpened:0}}
+  return loadedData
+}
+
+function setCurrentEpochDay(){
+  var msPerHour = 1000*60*60
+  var msPerDay =  msPerHour*24
+  var now = Date.now()
+  var surplus = now%msPerDay
+  var dayNow = now-surplus
+}
+
+new Vue({el: "#gratitude-body"})
