cd00e4bf94764aa4bfeb6b75b40a290185073a88
wolfram74 <phaugen@gmail.com>
2019-02-09 14:04:47 -0700

    dev vals hard coded

--- docs/scripts/runner.js
+++ docs/scripts/runner.js
@@ -1,23 +1,58 @@
 function load(defaults){
   var defaultVals = {
-    loggable:{},
+    loggables:[],
     settings:{daysReviewed:7}
   }
-  if(typeof defaults !== undefined){
+  var devVals = genDevVals()
+  if(typeof defaults === 'undefined'){var noDefaults=true}
+  if(!noDefaults){
     return defaultVals
   }
+  console.log('not default')
   var loadedData = JSON.parse(
     localStorage.getItem('dailyLoggerState')
-    ) || defaultVals
+    ) || devVals;
   return loadedData
 }
 
-function setCurrentEpochDay(){
-  var msPerHour = 1000*60*60
-  var msPerDay =  msPerHour*24
+function genDevVals(){
+  var msPerDay =  1000*60*60*24
   var now = Date.now()
   var surplus = now%msPerDay
   var dayNow = now-surplus
+  var twoDaysAgo = dayNow - msPerDay*2
+  return {
+    loggables:[
+      {
+        details:{
+          type:'string',
+          label:'test str1',
+          nullable:false,
+          categories: []
+        },
+        entries:[{date:twoDaysAgo, value:'fart noise'}]
+      },
+      {
+        details:{
+          type:'number',
+          label:'test num1',
+          nullable:true,
+          categories: []
+        },
+        entries:[{date:twoDaysAgo, value:6}]
+      },
+      {
+        details:{
+          type:'number',
+          label:'test num2',
+          nullable:false,
+          categories: []
+        },
+        entries:[{date:twoDaysAgo, value:6}]
+      },
+    ],
+    settings:{daysReviewed:7}
+  }
 }
 
 function mod(n,k){return ((n%k)+k)%k}
