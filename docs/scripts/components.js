559657b1e337dac0fdd8a2d38d489ea03c2e1d48
wolfram74 <phaugen@gmail.com>
2019-02-19 17:35:44 -0700

    header computed property and beginning of rows property set up

--- docs/scripts/components.js
+++ docs/scripts/components.js
@@ -18,7 +18,11 @@ Vue.component('gratitude-body', {
       >
       </current-entry-fields>
 
-      <review-old-entries>
+      <review-old-entries
+        v-bind:loggablesData = this.state.loggables
+        v-bind:epochDate = this.currentEpochDay
+        v-bind:settings = this.state.settings
+      >
       </review-old-entries>
     </div>
   `,
@@ -135,10 +139,67 @@ Vue.component('current-number',{
 })
 
 Vue.component('review-old-entries',{
+  props:['loggablesData','epochDate', 'settings'],
+/*
+strings get their own section, line per date, maybe a different number of columns based on width of screen
+numbers get column per entry
+fixing a row to having uniform height may require binding a row together
+*/
   template:`
   <div class='review-old-entries'>
     <h3> old entries table</h3>
+    <number-table
+      v-bind:numLogs = this.logsByType.number
+      v-bind:settings = this.settings
+      v-bind:epochDate = this.epochDate
+    >
+    </number-table>
   </div>
-  `
+  `,
+  computed:{
+      logsByType:function(){
+      var strings = []
+      var numbers = []
+      for(var index=0; index<this.loggablesData.length; index++){
+        var loggable = this.loggablesData[index]
+        if(loggable.details.type==='string'){
+          strings.push(loggable)
+        }else{numbers.push(loggable)}
+      }
+      return {string:strings, number:numbers}
+    }
 
+  }
+
+})
+
+Vue.component('number-table',{
+  props:['numLogs', 'epochDate','settings'],
+  template:`
+  <div>
+  hi, {{this.epochDate}}
+  </div>
+  `,
+  computed:{
+    headers: function(){
+      var out = ['day']
+      for(var index=0; index<this.numLogs.length; index++){
+        out.push(this.numLogs[index].details.label)
+      }
+      return out
+    },
+    rows: function(){
+      var out= []
+      console.log('tried')
+      for(var daysAgo = 0; daysAgo <= this.settings.daysReviewed; daysAgo+=1){
+        console.log(out)
+        var epochTime = this.epochDate-daysAgo*24*3600*1000
+        out.push(humanDateYYMMDD(epochTime))
+        for(var index=0;index<this.numLogs.length; index++){
+          var log = this.numLogs[index]
+        }
+      }
+      return out
+    }
+  }
 })
